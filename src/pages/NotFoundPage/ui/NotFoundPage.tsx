5e56d40a43716e8e9c0a22f7e14a39c52e394254
zakiquel <stronhold333@gmail.com>
2024-04-27 17:08:02 +0700

    not found page fix

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,5 +1,7 @@
+import cls from "./NotFoundPage.module.scss";
+
 export const NotFoundPage = () => (
-    <main>
+    <main className={cls.NotFoundPage}>
       <h1>Страница не найдена</h1>
     </main>
   )
